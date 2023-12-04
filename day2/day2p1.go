7e2b5ab0aa1037238700d590b6388fbb79143faf
Bryan Redd <reddb.rt@gmail.com>
2023-12-03 23:43:50 -0500

    Day2

---
+++ day2/day2p1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+// Max: 12 red cubes, 13 green cubes, and 14 blue cubes
+var colorMaxMap = map[string]int{"red": 12, "green": 13, "blue": 14}
+
+var re = regexp.MustCompile(`([0-9]+|red|green|blue)`)
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var sum int
+	Lines:
+	for scanner.Scan() {
+		parts := re.FindAllString(scanner.Text(), -1)
+		gameNum, err := strconv.Atoi(parts[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 2; i < len(parts); i+=2 {
+			val, err := strconv.Atoi(parts[i-1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			if val > colorMaxMap[parts[i]] {
+				continue Lines
+			}
+		}
+		sum += gameNum
+	}
+	fmt.Println(sum)
+}
+
---
+++ day2/day2p2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+var re = regexp.MustCompile(`([0-9]+|red|green|blue)`)
+
+var colorIndex = map[string]int{"red": 0, "green": 1, "blue": 2}
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var sumPowers int
+	for scanner.Scan() {
+		// [red, green, blue]
+		var cubeMax [3]int
+		parts := re.FindAllString(scanner.Text(), -1)
+		for i := 2; i < len(parts); i+=2 {
+			val, err := strconv.Atoi(parts[i-1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			if val > cubeMax[colorIndex[parts[i]]] {
+				cubeMax[colorIndex[parts[i]]] = val
+			}
+		}
+		sumPowers += (cubeMax[0] * cubeMax[1] * cubeMax[2])
+	}
+	fmt.Println(sumPowers)
+}
+
